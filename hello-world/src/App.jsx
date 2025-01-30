44adb8cec85c7505c3e51b3775e6e1a7b8f866cb
Idkhowtoplay <akbarzainuri29@gmail.com>
2025-01-30 16:47:11 +0700

    welll

--- hello-world/src/App.jsx
+++ hello-world/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Today i learn nothing day 16
+          Today i learn nothing day 17
         </p>
       </div>
       <p className="read-the-docs">
