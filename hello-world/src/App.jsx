6669a23b8364e55c395d9c3dde06e43bb50c8cb9
Henrietta <141384520+Idkhowtoplay@users.noreply.github.com>
2025-02-07 20:23:26 +0700

    Update App.jsx

--- hello-world/src/App.jsx
+++ hello-world/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Today i learn nothing day 24
+          Today i learn nothing day 25
         </p>
       </div>
       <p className="read-the-docs">
